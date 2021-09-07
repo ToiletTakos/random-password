59ed4197e9230706ee1cd080961146f833717218
Dan <dan.pandolfo@gmail.com>
2021-09-06 20:20:20 -0400

    initial prompt

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,5 +10,20 @@ function writePassword() {
 
 }
 
+var generatePassword = function() {
+
+  var passwordLength = window.prompt("Please enter the desired password length with a minimum of '8' and maximum of '128'.");
+
+  if (passwordLength >= 8 && passwordLength <= 128){
+    window.alert("correct")
+    console.log(passwordLength);
+  }
+  else {
+    window.alert("Sorry you need to provide a valid answer please try again.");
+    return generatePassword();
+  }
+}
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
